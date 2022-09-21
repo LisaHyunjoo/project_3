c065276a8679b36fda585286eedca1b702b9cf46
DanielJoung <djoung100@hotmail.com>
2022-09-21 18:50:12 -0400

    setting controllers, models

--- networgram-frontend/src/App.js
+++ networgram-frontend/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
-import Newpost from './Components/Wall/Newpost'
+import Newpost from "./Components/Wall/Newpost";
 import Comment from "./Components/Wall/Comment";
-import Profile from './Components/User/Profile'
+import Profile from "./Components/User/Profile";
 import Login from "./Components/User/Login";
 import Register from "./Components/User/Register";
 
-function App() {
-  return (
-    <>
-      < Newpost />
-      <Comment />
-      <Profile />
-      <Login />
-      <Register />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <Newpost />
+        <Comment />
+        <Profile />
+        <Login />
+        <Register />
+      </>
+    );
+  }
 }
 
 export default App;
