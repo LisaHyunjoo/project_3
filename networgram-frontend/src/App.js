19a1ffe40c1e825fa96a0ac9ed1182cb03893b9c
Lisa <lisahongofficial@gmail.com>
2022-09-21 14:29:46 -0400

    Comment page is created

--- networgram-frontend/src/App.js
+++ networgram-frontend/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Newpost from './Components/Wall/Newpost'
+import Comment from "./Components/Wall/Comment";
 import Profile from './Components/User/Profile'
 import Login from "./Components/User/Login";
+import Register from "./Components/User/Register";
 
 function App() {
   return (
     <>
       < Newpost />
+      <Comment />
       <Profile />
       <Login />
+      <Register />
     </>
   );
 }
