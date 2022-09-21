7755ed2f90259fb8cebb4b912b6729a767f85e44
Lisa <lisahongofficial@gmail.com>
2022-09-21 14:17:03 -0400

    Newpost is created

---
+++ networgram-frontend/src/Components/Wall/Newpost.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Newpost extends Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+            return (
+                <>
+                    <h1>New Post</h1>
+                </>                
+            )
+    }
+   
+
+}
