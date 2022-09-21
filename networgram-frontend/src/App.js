bb54a99fdcdecefa0b4fc08963be1036f9682e28
Lisa <lisahongofficial@gmail.com>
2022-09-21 14:26:41 -0400

    login page is created

--- networgram-frontend/src/App.js
+++ networgram-frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Newpost from './Components/Wall/Newpost'
 import Profile from './Components/User/Profile'
+import Login from "./Components/User/Login";
 
 function App() {
   return (
     <>
       < Newpost />
       <Profile />
+      <Login />
     </>
   );
 }
---
+++ networgram-frontend/src/Components/User/Login.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <>
+                <h1>Log in/Log out</h1>
+            </>
+        )
+    }
+}
+
+export default Login;
+
