62efafd3240f4abc2321aa4d1d293975a0421403
DanielJoung <djoung100@hotmail.com>
2022-09-21 19:36:10 -0400

    make post db

--- networgram-backend/controllers/PostCtrls.js
+++ networgram-backend/controllers/PostCtrls.js
@@ -11,9 +11,41 @@ const index = (req, res) => {
 };
 
 const create = (req, res) => {
-  db.Post;
+  db.Post.create(req.body, (err, createdPost) => {
+    if (err) return res.status(404).json({ error: err.message });
+    return res.status(200).json(createdPost);
+  });
+};
+
+const destroy = (req, res) => {
+  db.Post.findByIdAndDelete(req.params.id, (err, deletedPost) => {
+    if (!deletedPost) return res.status(404).json({ error: "Post not found" });
+    if (err) return res.status(404).json({ error: err.message });
+    return res.status(200).json({
+      message: "deleted",
+    });
+  });
+};
+
+const update = (req, res) => {
+  db.Post.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    {
+      new: true,
+    },
+    (err, updatedPost) => {
+      if (err) return res.status(404).json({ error: err.message });
+      return res.status(200).json(updatedPost);
+    }
+  );
 };
 
 module.exports = {
   index,
+  create,
+  destroy,
+  update,
 };
