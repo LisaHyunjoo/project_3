62d452b6477c1c56b0a256824ff10302b37238e1
DanielJoung <djoung100@hotmail.com>
2022-09-23 19:23:57 -0400

    finish user route

--- networgram-backend/routes/users.routes.js
+++ networgram-backend/routes/users.routes.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const ctrls = require("../controllers");
 
 // router
-
+router.get("/register", (req, res) => {});
 router.post("/register", ctrls.users.register);
+router.get("/signin", (req, res) => {});
 router.post("/signin", ctrls.users.signin);
+router.get("/signout", (req, res) => {
+  req.session.destroy();
+});
 
 module.exports = router;
