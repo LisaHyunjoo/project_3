bb4d1bac858a125c7ece397aba33b557bfbe29f7
DanielJoung <djoung100@hotmail.com>
2022-09-23 20:17:57 -0400

    add server on app.js

--- networgram-frontend/src/App.js
+++ networgram-frontend/src/App.js
@@ -6,10 +6,20 @@ import Profile from "./Components/User/Profile";
 import Login from "./Components/User/Login";
 import Register from "./Components/User/Register";
 
+let baseURL = "";
+
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3000";
+} else {
+  baseURL = "heroku backend url";
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      post: [],
+    };
   }
   render() {
     return (
