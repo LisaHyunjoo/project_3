6b05a532b93ee58328d048667a363d2f1e5ed104
DanielJoung <djoung100@hotmail.com>
2022-09-23 20:16:23 -0400

    fix user route

--- networgram-backend/routes/users.routes.js
+++ networgram-backend/routes/users.routes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const ctrls = require("../controllers");
 
 // router
-router.get("/register", (req, res) => {});
+router.get("/register", (req, res) => {
+  req.session.currenUser, req.query.error;
+});
 router.post("/register", ctrls.users.register);
-router.get("/signin", (req, res) => {});
+router.get("/signin", (req, res) => {
+  req.session.currentUser, req.query.error;
+});
 router.post("/signin", ctrls.users.signin);
 router.get("/signout", (req, res) => {
   req.session.destroy();
